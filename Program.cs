d7b109984f2a7a7c501f41ca6ac3a14dd6827db1
IkarosSQ <federleyj@gmail.com>
2023-11-20 13:17:56 +0100

    dsa

--- Program.cs
+++ Program.cs
@@ -9,8 +9,12 @@ namespace csharpfun
             Console.Title = "GLaDOS";
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Bitch");
+            Console.WriteLine("Hello, Aperture enrichment center");
+            Console.WriteLine("what would you like me to do?");
+            
+            Console.ReadLine();
+            Console.WriteLine("Sorry i dont know what you're wanting me to do.");
+
             Console.ReadKey();
         }
     }
