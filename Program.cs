0c377148bd428d488a2463ced567426880fc8703
IkarosSQ <federleyj@gmail.com>
2023-11-20 12:42:34 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace csharpfun 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "GLaDOS";
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bitch");
+            Console.ReadKey();
+        }
+    }
+}
